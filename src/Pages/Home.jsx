a5ad7ae168ce2db8049f55e9cbf20efd7cfb6344
ardiansyahdodii <dodyn890@gmail.com>
2023-10-19 10:50:01 +0700

    fetch data games

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
 import GenreList from "../Components/GenreList"
+import GlobalAPI from "../Services/GlobalAPI"
 
 const Home = () => {
+
+    const [allGamesList, setAllGamesList] = useState([])
+
+    useEffect(() =>{
+        GetAllGames()
+    },[])
+
+    const GetAllGames = () => {
+        GlobalAPI.getAllGames.then((res) =>{
+            console.log(res.data.results)
+            setAllGamesList(res.data.results)
+        })
+    }
     return (
         <div className="grid grid-cols-4 px-8">
             <div className="hidden md:block">
--- src/Services/GlobalAPI.jsx
+++ src/Services/GlobalAPI.jsx
@@ -6,7 +6,8 @@ const axiosCreate = axios.create({
 })
 
 const getGenreList = axiosCreate.get('/genres?key='+key)
+const getAllGames = axiosCreate.get('/games?key='+key)
 
 export default{
-    getGenreList
+    getGenreList, getAllGames
 }
