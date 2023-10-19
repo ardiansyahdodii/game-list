98ce499a72f5fe75c2c1d2e6cb72c8b404f6ac9a
ardiansyahdodii <dodyn890@gmail.com>
2023-10-19 10:34:56 +0700

    responsive genre list

--- src/Components/GenreList.jsx
+++ src/Components/GenreList.jsx
@@ -4,6 +4,7 @@ import GlobalAPI from '../Services/GlobalAPI'
 const GenreList = () => {
 
     const [genreList, setGenreList] = useState([])
+    const [activeIndex, setActiveIndex] = useState([])
 
     useEffect(() => {
         GetGenreList()
@@ -18,11 +19,15 @@ const GenreList = () => {
     return (
         <div>
             <h2 className='text-[30px] font-bold dark:text-white'>Genre</h2>
-            {genreList.map((item) => (
-                <div key={item.id} className='flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg group dark:hover:bg-gray-600 '>
+            {genreList.map((item, index) => (
+                <div key={item.id} onClick={() => setActiveIndex(index)}
+                className={`flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg group dark:hover:bg-gray-600
+                ${activeIndex == index ? " bg-gray-300 dark:bg-gray-600" : null}`}>
                     <img src={item.image_background} 
-                    className='w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300' />
-                    <h3 className='text-lg group-hover:font-bold dark:text-white'>{item.name}</h3>
+                    className={`w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300
+                    ${activeIndex == index ? " scale-105" : null}`} />
+                    <h3 className={`text-lg group-hover:font-bold dark:text-white
+                    ${activeIndex == index ? " font-bold" : null}`}>{item.name}</h3>
                 </div>
             ))}
         </div>
