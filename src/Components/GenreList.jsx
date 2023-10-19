e9f8a02066c511e98aec5927a67801bd56b3efdb
ardiansyahdodii <dodyn890@gmail.com>
2023-10-19 10:16:09 +0700

    tampilan genre game sidebar

--- src/Components/GenreList.jsx
+++ src/Components/GenreList.jsx
@@ -1,20 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import GlobalAPI from '../Services/GlobalAPI'
 
 const GenreList = () => {
 
-    useEffect(()=>{
+    const [genreList, setGenreList] = useState([])
+
+    useEffect(() => {
         GetGenreList()
     })
 
-    const GetGenreList = () =>{
-        GlobalAPI.getGenreList.then((res) =>{
+    const GetGenreList = () => {
+        GlobalAPI.getGenreList.then((res) => {
             console.log(res.data.results)
+            setGenreList(res.data.results)
         })
     }
-  return (
-    <div>GenreList -----</div>
-  )
+    return (
+        <div>
+            <h2 className='text-[30px] font-bold dark:text-white'>Genre</h2>
+            {genreList.map((item) => (
+                <div key={item.id} className='flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg group dark:hover:bg-gray-600 '>
+                    <img src={item.image_background} 
+                    className='w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300' />
+                    <h3 className='text-lg group-hover:font-bold dark:text-white'>{item.name}</h3>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default GenreList
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,8 @@ import GenreList from "../Components/GenreList"
 
 const Home = () => {
     return (
-        <div className="grid grid-cols-4">
-            <div className="bg-red-600 h-full hidden md:block">
+        <div className="grid grid-cols-4 px-8">
+            <div className="hidden md:block">
                 <GenreList />
             </div>
             <div className="col-span-4 md:col-span-3 bg-blue-400">Game List</div>
